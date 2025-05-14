66f014a8b8f420c48104c5a48634f0c17a5c8388
Hasan <balhaevhk@icloud.com>
2025-05-14 20:34:38 +0300

    configure logging with log/slog

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,19 +1,66 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"gRPC_service/internal/config"
+	"gRPC_service/internal/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
 )
 
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
+
+)
 func main() {
 	// TODO: инициализировать объкт конфига
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
 	// TODO: инициализировать логгер
+	log := setupLogger(cfg.Env)
 
+	log.Info("starting application", 
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("port", cfg.GRPC.Port),
+	)
+	log.Debug("debug message")
+	log.Error("error message")
+	log.Warn("warn message")
 	// TODO: инициализировать приложение (app)
 
 	// TODO: запустить gRPC-сервер приложения
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+opts := slogpretty.PrettyHandlerOptions{
+	SlogOpts: &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	},
+}
+
+handler := opts.NewPrettyHandler(os.Stdout)
+return slog. New(handler)
 }
--- go.mod
+++ go.mod
@@ -2,12 +2,20 @@ module gRPC_service
 
 go 1.24.0
 
+require github.com/ilyakaznacheev/cleanenv v1.5.0
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
+
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
-	// github.com/balhaevhk/protos v0.0.1 // indirect
 	github.com/balhaevhk/protos v0.0.1 // indirect
-	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/fatih/color v1.18.0
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
