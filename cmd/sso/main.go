e14b3baa1f1a02d0ce386730293e17bfe0035dec
Hasan <balhaevhk@icloud.com>
2025-05-14 11:17:21 +0300

    application configuration

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,12 +1,19 @@
-package sso
+package main
+
+import (
+	"fmt"
+	"gRPC_service/internal/config"
+)
 
 func main() {
 	// TODO: инициализировать объкт конфига
+	cfg := config.MustLoad()
 
+	fmt.Println(cfg)
 	// TODO: инициализировать логгер
-	
+
 	// TODO: инициализировать приложение (app)
-	
+
 	// TODO: запустить gRPC-сервер приложения
 
 }
